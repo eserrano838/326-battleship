3d60671abe823b38475c9f3e6345aa0d5d0d7be9
hbrender <38145008+hbrender@users.noreply.github.com>
2019-04-08 00:26:47 -0700

    basics/playing around

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BattleShip
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BattleShip
+{
+    public class Project
+    {
+        public static void Main(String[] args)
+        {
+            string player1name;
+            string player2name;
+
+            Console.WriteLine("Welcome to BattleShip!");
+
+            Console.Write("Please enter Player 1's name: ");
+            player1name = Console.ReadLine();
+
+            Console.Write("Please enter Player 2's name: ");
+            player2name = Console.ReadLine();
+
+            Player player1 = new Player(player1name);
+            Player player2 = new Player(player2name);
+
+            Console.Write(player1.Name);
+            Console.Write(player2.Name);
+
+        }
+    }
+}
